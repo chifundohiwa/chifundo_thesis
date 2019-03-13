5b054ecffa0410bcd2428cdc263a7b466c12c6c4
chifundohiwa <chifundohiwa@gmail.com>
2019-03-13 06:59:42 +0000

    modified

--- 01_Alignment.sh
+++ 01_Alignment.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-dir_fastq=PATH_TO/fastq
-dir_Output=PATH_TO/results
-dir_Hg38=PATH_TO/hg38bundle
+dir_fastq=/root/chifundo_thesis/fastq
+dir_Output=/root/chifundo_thesis/results
+dir_Hg38=/root/chifundo_thesis/hg38bundle
 
 mkdir -p ${dir_Output}/$1
 mkdir -p ${dir_Output}/$1/{LOG,BAM}
 
-docker run --rm -v ${dir_fastq}:/fastq -v ${dir_Hg38}:/reference biocontainers/bwa \
+docker run --rm -v ${dir_fastq}:/fastq -v ${dir_Hg38}:/reference biocontainers/bwa:v0.7.15_cv3 \
 	bwa mem \
-	-t 16 \
+	-t 7 \
 	-R "@RG\tID:$1\tSM:$1\tPL:Illumina\tLB:WES" \
-	/reference/Homo_sapiens_assembly38.fasta.gz \
-	/fastq/$1_Sample_$2/$2_combined_R1.fastq.gz \
-	/fastq/$1_Sample_$2/$2_combined_R2.fastq.gz \
+	/reference/chr19_chr19_KI270866v1_alt.fasta \
+	/fastq/$1.read1.fq \
+	/fastq/$1.read2.fq \
 	| samtools sort -o ${dir_Output}/$1/BAM/$1_sorted.bam
